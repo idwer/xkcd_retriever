6f2b306612a4ff7716cc97001465a277d6dfc446
Idwer <idwer@localhost>
2025-08-24 15:51:06 +0200

    apply suggestions made by `cargo clippy --fix`

--- src/routes/http_404_handler.rs
+++ src/routes/http_404_handler.rs
@@ -4,6 +4,6 @@ use actix_web::{Error, HttpResponse};
 pub async fn handle_404() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::build(StatusCode::NOT_FOUND)
            .content_type("text/html; charset=utf-8")
-           .body(format!(r#"<html><body><img src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg"></html></body>"#))
+           .body(r#"<html><body><img src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg"></html></body>"#.to_string())
     )
 }
