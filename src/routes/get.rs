6b9d5969f4e0991d78b3890c4a8f3cba2da30d56
Idwer <idwer@localhost>
2025-10-15 20:44:20 +0200

    use HTTPResponse::build() in src/routes/get.rs src/routes/http_404_handler.rs

--- src/routes/get.rs
+++ src/routes/get.rs
@@ -1,7 +1,10 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::http::StatusCode;
+use actix_web::HttpResponse;
 
-pub async fn get_form() -> impl Responder {
-    HttpResponse::Ok().content_type("text/html").body(
+pub async fn get_form() -> HttpResponse {
+    HttpResponse::build(StatusCode::OK)
+    .content_type("text/html")
+    .body(
         r#"<html><body>
             <form action="/xkcd" method="POST">
             <p>XKCD ID: <input name = "id" type = "number" value=200 /></p>
